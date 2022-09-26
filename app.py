4cfa6a83e750688019b144ba71e9a116823aa265
mikey-esteban <michaelp.esteban@gmail.com>
2022-09-26 13:29:20 -0400

    created line graph

---
+++ app.py
@@ -0,0 +1,52 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import csv
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+sales = []
+dates = []
+region = []
+
+with open('./data/pink_morsel_sales.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        sales.append(row[0])
+        dates.append(row[1])
+        region.append(row[2])
+
+df = pd.DataFrame({
+    "Sales": sales,
+    "Dates": dates,
+    "Region": region
+})
+
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+fig = px.line(df, x="Dates", y="Sales", color="Region")
+
+app.layout = html.Div(children=[
+    html.H1(children='Pink Morsel Sales by Date'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
